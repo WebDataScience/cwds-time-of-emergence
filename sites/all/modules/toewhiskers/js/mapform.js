0f8116639e3a79ef6bfeee3ad51ea7f9d6566a5b
Dwaine Trummert <dwainet@uw.edu>
2014-12-03 15:04:21 -0800

    More work on the map choosing logic.

--- sites/all/modules/toewhiskers/js/mapform.js
+++ sites/all/modules/toewhiskers/js/mapform.js
@@ -1,34 +1,14 @@
-
 jQuery( document ).ready(function( $ ) {
 
   
-      
-      
-  // Hide/show additional form components as necessary.
-  $('input[name=theme]:radio').click(function(){
-  
-    if($('input:radio[name=compare]:checked').val() == '0'){ 
-      $('div.form-item-geodomain').show(); 
-      $('div.form-item-region').show(); 
-    }
-    if($('input:radio[name=compare]:checked').val() == '1'){ 
-      $('div.form-item-geodomain').hide(); 
-      $('div.form-item-region').hide(); 
-    }
-    if($('input:radio[name=compare]:checked').val() == '2'){ 
-      $( "#toewhiskers-explore-form" ).submit();
-      return;
-    }
-    //enable 'region' dropdown.
-    // this solution requires newer version of jQuery: $("#edit-region").prop("disabled", false);
-    //$('#edit-region').removeAttr('disabled');
-    //$('div.form-item-region').show(); 
-    //$('div.form-item-geodomain').show();   
-    $('#edit-submit').removeAttr('disabled');  
+  $('#edit-filter').click(function(){
+ 
+    //alert($('input:radio[name=theme]:checked').val());
+    var themeindex = $('input:radio[name=theme]:checked').val();
+    var impactindex = $('input:radio[name=impact]:checked').val();
+    var averageextremeindex = $('input:radio[name=descriptor]:checked').val();
     
-
-    
-    $.getJSON( 'map/variablelist', function ( options ) { 
+    $.getJSON( 'map/variablelist/' + themeindex + '/' + impactindex + '/' + averageextremeindex, function ( options ) { 
       //alert ( options );
       $('#edit-variable').empty();
       $.each(options, function(val, text) {
@@ -36,11 +16,8 @@ jQuery( document ).ready(function( $ ) {
             $('<option></option>').val(val).html(text)
           );
       });
-    });
-    
-    
-    
-  });
-        
-        
+    });   
+    return(false); 
+  });  
+  
 }); 
