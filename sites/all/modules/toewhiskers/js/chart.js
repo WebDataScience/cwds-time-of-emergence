227410bbcc38a5bc67798e4ad97973c63fb9563e
Dwaine Trummert <dwainet@uw.edu>
2015-02-19 12:06:30 -0800

    Updating TOE database and adding dataset to timeline variables

--- sites/all/modules/toewhiskers/js/chart.js
+++ sites/all/modules/toewhiskers/js/chart.js
@@ -16,15 +16,12 @@ jQuery( document ).ready(function( $ ) {
   dataset['BCSD5'] = 'Statistically-downscaled (CMIP5 BCSD)';
   websitetextarray['dataset'] = dataset;
   var emission = new Object;
-  //emission['rcp45'] = 'RCP4.5';
-  //emission['rcp85'] = 'RCP8.5';
   
   emission['low'] = 'Low (RCP4.5 or B1)';
   emission['high'] = 'High (RCP8.5 or A1B)';
   
   websitetextarray['emission'] = emission;
   websitetextarray['region'] = jQuery( "#region" ).html();
-
 
   dataarray = []; 
   jsondataarray = [];
@@ -34,7 +31,6 @@ jQuery( document ).ready(function( $ ) {
   colorarray = ["#E59C00","#E39400","#E18C00","#DF8400","#DD7C00","#DB7400","#D96C00","#D76400","#D55C00","#D35400","#D14C01","#CF4401","#CD3C01","#CB3401","#C92C01","#C72401","#C51C01","#C31401","#C10C01","#C00402"];
 
 
-  
   // Ajax endpoint will retrieve vars and parameters from session.
   $.post( "timelinedata" , function( jsonobj ) { 
   
@@ -50,7 +46,8 @@ jQuery( document ).ready(function( $ ) {
        dataarray.push([onerow[2],onerow[1],'opacity: 0',1,'opacity: 1']);
        //jsondataarray.push([onerow[2],onerow[1],onerow[2]]);
        jsondataarray.push([ onerow[2],onerow[4], colorarray.shift() ]);
-       timelinedataarray.push([ onerow[4] , 0, onerow[2], onerow[5], onerow[6], 0]);
+       // onerow[7] = dataset
+       timelinedataarray.push([ onerow[4] , 0, onerow[2], onerow[5], onerow[6], 0, onerow[7] ]);
        /* val 0 at [5] represents default vertical height between the axis and variable label */ 
     }
 
@@ -124,7 +121,7 @@ function dateConversion(onerow) {
 }
   
 //function drawD3Timeline(timelinedataarray, maxtoeyear,confidence,tolerance,dataset,region, emission)  
-function drawD3Timeline(timelinedataarray, maxtoeyear,confidence,tolerance,dataset, region){
+function drawD3Timeline(timelinedataarray,maxtoeyear,confidence,tolerance,dataset,region){
  var data = [[2000,0,"tooltip1","label1","rcp45",50],
 	     [2020,0,"tooltip2","label2","rcp85",50],
 	     [2085,0,"tooltip3","label3","rcp45",50],
@@ -133,25 +130,25 @@ function drawD3Timeline(timelinedataarray, maxtoeyear,confidence,tolerance,datas
  var chartheight = 280;
  data = timelinedataarray;
 
-/* Code to prevent label text overlapping */
+  /* Code to prevent label text overlapping */
 
-var temparr = [[0,0]];
+  var temparr = [[0,0]];
 
-for (var i = 0; i < data.length; i++) {
-	var found = false;
-	for (var j = 0; j < temparr.length; j++) {
-		if (data[i][0] == temparr[j][0]) {
-			var count = temparr[j][1];
-			data[i][5] = data[i][5] + (5 + count * 10);
-			temparr[j][1] = count + 1;
-			found = true;
-		}
-	}
+  for (var i = 0; i < data.length; i++) {
+    var found = false;
+    for (var j = 0; j < temparr.length; j++) {
+      if (data[i][0] == temparr[j][0]) {
+        var count = temparr[j][1];
+        data[i][5] = data[i][5] + (5 + count * 10);
+        temparr[j][1] = count + 1;
+        found = true;
+      }
+    }
 
-	if (found == false) {
-		temparr.push([data[i][0], 1]);
-	}
-} 
+    if (found == false) {
+      temparr.push([data[i][0], 1]);
+    }
+  } 
 		   
 		var margin = {top: 120, right: 40, bottom: 40, left: 30}
 		  , width = 940 - margin.left - margin.right
@@ -166,7 +163,6 @@ for (var i = 0; i < data.length; i++) {
 		var y = d3.scale.linear()
 		        .domain([0, d3.max(data, function(d) { return d[1]; })])
 		        .range([ height, 0 ]);
-
 		      
 		var chart = d3.select('#timeline-chart')
 		.append('svg:svg')
@@ -179,7 +175,6 @@ for (var i = 0; i < data.length; i++) {
     .attr("height", "100%")
     .attr('border', 0)
     .attr("fill", "black");
-    
     chart.append("text")
         .attr("x", 15)             
         .attr("y", 20)
@@ -201,14 +196,13 @@ for (var i = 0; i < data.length; i++) {
         .style('font-size', '14px')
         .style('font-family', 'sans-serif')
         .text(dataset);
-
-    chart.append("text")
-        .attr("x", 15)             
-        .attr("y", 80)
-        .style("stroke", "white")
-        .style('font-size', '14px')
-        .style('font-family', 'sans-serif')
-        .text(region);
+  chart.append("text")
+    .attr("x", 15)             
+    .attr("y", 80)
+    .style("stroke", "white")
+    .style('font-size', '14px')
+    .style('font-family', 'sans-serif')
+    .text(region);
 	
     /*
     chart.append("svg:image")
@@ -219,86 +213,86 @@ for (var i = 0; i < data.length; i++) {
         .attr("height", "105");
     */
     
-		var main = chart.append('g')
-		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-		.attr('width', width)
-		.attr('height', height)
-		.attr('class', 'main')   
+  var main = chart.append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('class', 'main')   
 		    
-		// draw the x axis
-		var xAxis = d3.svg.axis()
-		.scale(x)
-		.orient('bottom')
-		.ticks(20)
-		.tickFormat(format)
-		.tickSize(15,-1);
+  // draw the x axis
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom')
+    .ticks(20)
+    .tickFormat(format)
+    .tickSize(15,-1);
 
-		main.append('g')
-		.attr('transform', 'translate(0,' + height + ')')
-		.attr('class', 'main axis date main-axis-date')
-		.call(xAxis);
-    
-    // Attributes for the xAxis.
-    d3.select('.axis')
-      .attr("stroke", "white")
-      .attr("stroke-width", 2)
-      .attr('shape-rendering','crispEdges');
+  main.append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .attr('class', 'main axis date main-axis-date')
+    .call(xAxis);
+  
+  // Attributes for the xAxis.
+  d3.select('.axis')
+    .attr("stroke", "white")
+    .attr("stroke-width", 2)
+    .attr('shape-rendering','crispEdges');
          
-		var g = main.append("svg:g"); 
+  var g = main.append("svg:g"); 
 
-		var node = g.selectAll("g")
-                .data(data)
-                .enter()
-                .append("g");         
+  var node = g.selectAll("g")
+              .data(data)
+              .enter()
+              .append("g");         
         
-    // vertical variable line.        
-		node.append("line")
-		  .attr("x1", function (d,i) { return x(d[0]); } )
-      .attr("y1", function(d,i) { return d[5]; })
-			.attr("x2", function (d,i) { return x(d[0]); } )
-			.attr("y2", 120)
-			.attr("stroke-width", 1)
-			.style("shape-rendering", "crispEdges")
-      .attr('opacity', 1)
-			.attr("stroke", "white");
+  // vertical variable line.        
+  node.append("line")
+    .attr("x1", function (d,i) { return x(d[0]); } )
+    .attr("y1", function(d,i) { return d[5]; })
+    .attr("x2", function (d,i) { return x(d[0]); } )
+    .attr("y2", 120)
+    .attr("stroke-width", 1)
+    .style("shape-rendering", "crispEdges")
+    .attr('opacity', 1)
+    .attr("stroke", "white");
 
-		node.append("line:circle")
-		  .attr("cx", function (d,i) { return x(d[0]); } )
-		  .attr("cy", function (d) { return y(d[1]); } )
-      .attr("fill", "#248F24")
-      .attr("stroke", "#99FF99")
-		  .attr("r", "6");
+  node.append("line:circle")
+    .attr("cx", function (d,i) { return x(d[0]); } )
+    .attr("cy", function (d) { return y(d[1]); } )
+    .attr("fill", "#248F24")
+    .attr("stroke", "#99FF99")
+    .attr("r", "6");
 
-    // Hide some of the vertical line as necessary with a black rectangle.
-		node.append("line:rect") 
-      .attr("x", function (d,i) { return x(d[0]) - 5; } )
-      .attr("y", function (d) { return d[5] - 10; } )
-      .attr("height", "15")
-      .attr("width", "10")
-      .attr("fill", "#070707");
+  // Hide some of the vertical line as necessary with a black rectangle.
+  node.append("line:rect") 
+    .attr("x", function (d,i) { return x(d[0]) - 5; } )
+    .attr("y", function (d) { return d[5] - 10; } )
+    .attr("height", "15")
+    .attr("width", "10")
+    .attr("fill", "#070707");
 
-    // Add variable short names.
-		node.append("line:text")
-		  .attr("x", function (d,i) { return x(d[0]); } ) 
-		  .attr("y", function (d) { return d[5]; } )
-		  .style("stroke", "white")
-      .style('font-size', '11px')
-      .style('font-family', 'sans-serif')
-      .attr("stroke-width", .3)
-		  .style("text-anchor", "middle")
-		  .text(function(d) { return d[3]; })
-		  .attr('title', function(d) {  return d[2]; })
-		  .attr('alt', function(d) { return d[2]; })
+  // Add variable short name - dataset
+	node.append("line:text")
+    .attr("x", function (d,i) { return x(d[0]); } ) 
+    .attr("y", function (d) { return d[5]; } )
+    .style("stroke", "white")
+    .style('font-size', '11px')
+    .style('font-family', 'sans-serif')
+    .attr("stroke-width", .3)
+    .style("text-anchor", "middle")
+    .text(function(d) { return d[3]+' - '+d[6]; })
+    .attr('title', function(d) {  return d[2]; })
+    .attr('alt', function(d) { return d[2]; })
 		.on("click", function(d) {
 		document.location.href="/boxplot" }); 
     
-    // Attributes for all other text.
-    d3.selectAll('text')
-      .attr('fill', 'white')
-      .style('font-size', '11px')
-      .attr("stroke-width", .3)
-      .style('font-family', 'sans-serif')
-			.style('text-shadow', 'none');
+  // Attributes for all other text.
+  d3.selectAll('text')
+  .attr('fill', 'white')
+  .style('font-size', '11px')
+  .attr("stroke-width", .3)
+  .style('font-family', 'sans-serif')
+  .style('text-shadow', 'none');
 
 }
 
